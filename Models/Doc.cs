9a78bd2ab04776e5f0f672bcb668ffe72c0fd616
Caio Lagreca <caiomiranda701@hotmail.com>
2024-08-29 20:52:34 +1000

    first comit

---
+++ Models/Doc.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewTextEditorProject.Models
+{
+    public class Doc
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Context { get; set; }
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; }
+    }
+}
